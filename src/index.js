4df57e92eec1d9d1ca664cc5281be551ff02c88d
d-mv <d.dash.mv@gmail.com>
2019-03-17 11:47:30 +0200

    Up and ready

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app";
+
+import "../assets/stylesheets/application.scss";
+
+if (root) {
+  ReactDOM.render(<App />, document.getElementById("root"));
+}
